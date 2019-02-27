6c2be0122a53bfc0a71746502d243fde8e8b6317
Le Van Thinh <vanthinhit310@gmail.com>
2019-02-27 12:56:41 +0700

    commit 004

---
+++ laravel/blog/database/migrations/2019_02_27_052809_create_user_tables.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_tables', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('username');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_tables');
+    }
+}
